8cdf2033bd190f89494d26d3b5fdb25458e0025f
stevethewright <stevethewright2@gmail.com>
2025-07-19 12:43:14 +0930

    Testing FastF1 EventSchedule
    Test endpoint currently for next race session just getting date time
    Updated python to run in dev mode in dockerfile & compose in reload
    mode.

--- backend/src/api/routes/events.py
+++ backend/src/api/routes/events.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter
+import fastf1
+from datetime import datetime
 
 router = APIRouter()
+schedule = fastf1.get_event_schedule(datetime.now().year)
 
 @router.get("/next")
 def get_next_race():
-  return {"test": "next_race"}
+  return {"date": schedule.get_event_by_round(13).get_session_date('FP1')} 
