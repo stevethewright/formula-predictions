019b1466f1bd3c68f75e29046db2a29a8dbb2db6
stevethewright <stevethewright2@gmail.com>
2025-08-15 21:05:40 +0930

    Ignoring pylance warning

--- backend/src/models/pydantic/CamelCaseModel.py
+++ backend/src/models/pydantic/CamelCaseModel.py
@@ -2,7 +2,7 @@ from pydantic.alias_generators import to_camel
 from sqlmodel import SQLModel
 
 class CamelCaseModel(SQLModel):
-  class Config(): # type: ignore # TODO: Anyway to do this better?
+  class Config(): # type: ignore
     alias_generator = to_camel
     validate_by_name = True 
     from_attributes = True
